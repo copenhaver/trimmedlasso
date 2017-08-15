d72b86fc8c528869d7b7feeff1231a5e22a86af2
copenhaver <mcopen@mit.edu>
2017-08-15 11:34:18 -0400

    file fixes

--- MATLAB/tl_apx_envelope.m
+++ MATLAB/tl_apx_envelope.m
@@ -4,60 +4,29 @@
 %%%
 
 
-function [betar] = tl_apx_envelope(p,k,y,X,mu,lambda,tol)
+function [betar] = tl_apx_envelope(p,k,y,X,mu,lambda)
 
-    % check argument count (final argument tol is optional
-    if (nargin ~= 6) && (nargin ~=7)
+    % throwbinding is considered an optional final argument
+    if nargin ~= 6
         disp('Incorrect number of arguments provided. Halting execution.');
         return;
-    else
-        if (nargin == 6)
-            tol = 1e-3;
-        end
-    end    
+    end
 
     if ( p ~= size(X,2) )
         disp('Specified p is not equal to row dimension of X. Halting execution.');
         return;
     end
-    
-    % perform alternating minimization in gamma and betar until convergence
-    % criteria is satisfied
-    
-    % initial beta with random value
-    
-    betar = randn(p,1);
-    gammar = zeros(p,1);
-    
-    prev_imp = Inf;
-    prev_obj = Inf;
-    cur_obj = Inf;
-    
-    while (prev_imp > tol)
-        
-        %% wrt gammar (betar fixed)
-        % ***N.B.***: We do not include the special cases as detailed in
-        % BCM17 in Appendix C. These are included in the julia
-        % implementation, but not here because we want the Matlab
-        % implementation to be as rudimentary as possible.
-        
-        % need to sort entries of betar
-        
-        
-        %% wrt betar (gammar fixed)
-        
-        res = sortrows([abs(betar),1:p]);
 
-        cvx_begin
-            variable gammar(p)
-            variable betar(p)
-            variable envelop
-            minimize( 0.5*betar'*X'*X*betar - y'*X*betar+dot(y,y)/2 + mu*sum(gammar)+ lambda*envelop )
-            subject to
-                envelop >= 0;
-                gammar >= 0;
-                gammar >= betar;
-                gammar >= -betar;
-                envelop >= sum(gammar) - k;
-        cvx_end
+    cvx_begin
+        variable gammar(p)
+        variable betar(p)
+        variable envelop
+        minimize( 0.5*betar'*X'*X*betar - y'*X*betar+dot(y,y)/2 + mu*sum(gammar)+ lambda*envelop )
+        subject to
+            envelop >= 0;
+            gammar >= 0;
+            gammar >= betar;
+            gammar >= -betar;
+            envelop >= sum(gammar) - k;
+    cvx_end
 end
